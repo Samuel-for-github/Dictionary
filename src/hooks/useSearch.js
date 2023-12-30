37149c23266d01b8f459ccac8f34565f67f7741d
Samuel fernandes <samuelfernandes10001@gamil.com>
2023-12-31 03:33:22 +0530

    api key

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -8,7 +8,7 @@ const useSearch = (search)=>{
     const [data, setData] = useState([])
 
 useEffect(()=>{  let url = `https://api.api-ninjas.com/v1/dictionary?word=${search}`
-fetch(url, {headers: {'X-Api-Key': 'NFSirjD0eqolLXk6nYhFE427Jr2PVD4LM11SmQML'}})
+fetch(url, {headers: {'X-Api-Key': 'PuaznLQ8l9JT4CzArvb58QbEgH9tV5WhNp0XnOgz'}})
     .then((res)=> res.json())
     .then((res)=> setData(res))
     
